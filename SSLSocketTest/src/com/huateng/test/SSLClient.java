093a13d7e535dad0338a851dde56f1129fca5dcc
smshen <306963591@qq.com>
2014-05-29 14:39:28 +0800

    代码格式化

--- SSLSocketTest/src/com/huateng/test/SSLClient.java
+++ SSLSocketTest/src/com/huateng/test/SSLClient.java
@@ -19,23 +19,22 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
-
 class SSLClient {
 
 	private SSLSocket socket = null;
-	
+
 	class MyX509TrustManager implements X509TrustManager {
 
 		@Override
 		public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
@@ -43,72 +42,72 @@ class SSLClient {
 			// TODO Auto-generated method stub
 			return null;
 		}
-		
+
 	}
-	
+
 	/**
 	 * 创建SSLSocket 不验证服务器证书
+	 * 
 	 * @throws Exception
 	 */
 	public void createNotVerifySSLSocket() throws Exception {
-		 // key store相关信息  
-	    String keyName = "sslclientkeys";  
-	    char[] keyStorePwd = "123456".toCharArray();  
-	    char[] keyPwd = "123456".toCharArray();  
-	    KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());  
-	  
-	    // 装载当前目录下的key store. 可用jdk中的keytool工具生成keystore  
-	    InputStream in = null;
-	    in = SSLServer.class.getClassLoader().getResourceAsStream(keyName);
-	    keyStore.load(in, keyPwd);  
-	    in.close();
-	  
-	    // 初始化key manager factory  
-	    KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory  
-	            .getDefaultAlgorithm());  
-	    kmf.init(keyStore, keyPwd);  
-	  
-	    // 初始化ssl context  
-	    SSLContext context = SSLContext.getInstance("TLS");
-	    context.init(kmf.getKeyManagers(),  
-	            new TrustManager[] { new MyX509TrustManager() },  
-	            new SecureRandom());  
-	  
-	    // 监听和接收客户端连接  
-	    SSLSocketFactory factory = context.getSocketFactory();
-		
+		// key store相关信息
+		String keyName = "sslclientkeys";
+		char[] keyStorePwd = "123456".toCharArray();
+		char[] keyPwd = "123456".toCharArray();
+		KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+
+		// 装载当前目录下的key store. 可用jdk中的keytool工具生成keystore
+		InputStream in = null;
+		in = SSLServer.class.getClassLoader().getResourceAsStream(keyName);
+		keyStore.load(in, keyPwd);
+		in.close();
+
+		// 初始化key manager factory
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+		kmf.init(keyStore, keyPwd);
+
+		// 初始化ssl context
+		SSLContext context = SSLContext.getInstance("TLS");
+		context.init(kmf.getKeyManagers(), new TrustManager[] { new MyX509TrustManager() }, new SecureRandom());
+
+		// 监听和接收客户端连接
+		SSLSocketFactory factory = context.getSocketFactory();
+
 		// 通过套接字工厂，获取一个客户端套接字
-//		SSLSocketFactory socketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+		// SSLSocketFactory socketFactory = (SSLSocketFactory)
+		// SSLSocketFactory.getDefault();
 		socket = (SSLSocket) factory.createSocket("localhost", 7070);
 	}
-	
+
 	/**
 	 * 创建SSLSocket 不验证服务器证书
+	 * 
 	 * @throws Exception
 	 */
 	public void createVerifySSLSocket() throws Exception {
 		String host = "localhost";
-        int port = 7070;
-        String keyf = "sslclientkeys";
-        String trustf = "sslclienttrust";
-        String pass = "123456";
-        
-        SSLContext ctx = SSLContext.getInstance("TLS");
-        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
-        KeyStore ks = KeyStore.getInstance("JKS");
-        KeyStore tks = KeyStore.getInstance("JKS");
-        
-        InputStream keyfin = SSLServer.class.getClassLoader().getResourceAsStream(keyf);
-        InputStream trustfin = SSLServer.class.getClassLoader().getResourceAsStream(trustf);
-        ks.load(keyfin, pass.toCharArray());
-        tks.load(trustfin, pass.toCharArray());
-        kmf.init(ks, pass.toCharArray());
-        tmf.init(tks);
-        ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
-        SSLSocketFactory ssf = ctx.getSocketFactory();
-        
-        socket = (SSLSocket) ssf.createSocket(host, port);
+		int port = 7070;
+		String keyf = "sslclientkeys";
+		String trustf = "sslclienttrust";
+		String pass = "123456";
+
+		SSLContext ctx = SSLContext.getInstance("TLS");
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+		TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+		KeyStore ks = KeyStore.getInstance("JKS");
+		KeyStore tks = KeyStore.getInstance("JKS");
+
+		InputStream keyfin = SSLServer.class.getClassLoader().getResourceAsStream(keyf);
+		InputStream trustfin = SSLServer.class.getClassLoader().getResourceAsStream(trustf);
+		ks.load(keyfin, pass.toCharArray());
+		tks.load(trustfin, pass.toCharArray());
+		kmf.init(ks, pass.toCharArray());
+		tmf.init(tks);
+		ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+		SSLSocketFactory ssf = ctx.getSocketFactory();
+
+		socket = (SSLSocket) ssf.createSocket(host, port);
 	}
 
 	public SSLClient() throws Exception {
--- SSLSocketTest/src/com/huateng/test/SSLServer.java
+++ SSLSocketTest/src/com/huateng/test/SSLServer.java
@@ -24,19 +24,19 @@ class SSLServer {
 	private static final String SECRET_CONTENT = "中国好 This is confidential content from server X, for your eye!";
 
 	private SSLServerSocket serverSocket = null;
-	
+
 	class MyX509TrustManager implements X509TrustManager {
 
 		@Override
 		public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
@@ -44,119 +44,119 @@ class SSLServer {
 			// TODO Auto-generated method stub
 			return null;
 		}
-		
+
 	}
-	
+
 	/**
 	 * 创建SSLServerSocket 不验证客户端证书
+	 * 
 	 * @throws Exception
 	 */
 	public void createNotVerifySSLServerSocket() throws Exception {
 		// 通过套接字工厂，获取一个服务器端套接字
-		 // key store相关信息  
-	    String keyName = "sslserverkeys";  
-	    char[] keyStorePwd = "123456".toCharArray();  
-	    char[] keyPwd = "123456".toCharArray();  
-	    KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());  
-	  
-	    // 装载当前目录下的key store. 可用jdk中的keytool工具生成keystore  
-	    InputStream in = null;
-	    in = SSLServer.class.getClassLoader().getResourceAsStream(keyName);
-	    keyStore.load(in, keyPwd);  
-	    in.close();
-	  
-	    // 初始化key manager factory  
-	    KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory  
-	            .getDefaultAlgorithm());  
-	    kmf.init(keyStore, keyPwd);  
-	  
-	    // 初始化ssl context  
-	    SSLContext context = SSLContext.getInstance("TLS");  
-	    context.init(kmf.getKeyManagers(),  
-	            new TrustManager[] { new MyX509TrustManager() },  
-	            new SecureRandom());  
-	  
-	    // 监听和接收客户端连接  
-	    SSLServerSocketFactory factory = context.getServerSocketFactory();
-		
-//		SSLServerSocketFactory socketFactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+		// key store相关信息
+		String keyName = "sslserverkeys";
+		char[] keyStorePwd = "123456".toCharArray();
+		char[] keyPwd = "123456".toCharArray();
+		KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+
+		// 装载当前目录下的key store. 可用jdk中的keytool工具生成keystore
+		InputStream in = null;
+		in = SSLServer.class.getClassLoader().getResourceAsStream(keyName);
+		keyStore.load(in, keyPwd);
+		in.close();
+
+		// 初始化key manager factory
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+		kmf.init(keyStore, keyPwd);
+
+		// 初始化ssl context
+		SSLContext context = SSLContext.getInstance("TLS");
+		context.init(kmf.getKeyManagers(), new TrustManager[] { new MyX509TrustManager() }, new SecureRandom());
+
+		// 监听和接收客户端连接
+		SSLServerSocketFactory factory = context.getServerSocketFactory();
+
+		// SSLServerSocketFactory socketFactory = (SSLServerSocketFactory)
+		// SSLServerSocketFactory.getDefault();
 		serverSocket = (SSLServerSocket) factory.createServerSocket(7070);
 	}
-	
+
 	/**
 	 * 创建SSLServerSocket 验证客户端证书
+	 * 
 	 * @throws Exception
 	 */
-	public void createVerifySSLServerSocket()  throws Exception {
-		String type = "TLS";//类型
-        String keyf = "sslserverkeys";//key文件路径
-        String trustf = "sslservertrust";//信任证书库
-        String pass = "123456";//密码
-        int port = 7070;//端口
-        //初始化上下文
-        SSLContext ctx = SSLContext.getInstance(type);
-        
-        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
-        
-        KeyStore ks = KeyStore.getInstance("JKS");
-        KeyStore tks = KeyStore.getInstance("JKS");
-        
-        //载入keystore
-        InputStream keyfin = SSLServer.class.getClassLoader().getResourceAsStream(keyf);
-        InputStream trustfin = SSLServer.class.getClassLoader().getResourceAsStream(trustf);
-        ks.load(keyfin, pass.toCharArray());
-        tks.load(trustfin, pass.toCharArray());
-        
-        kmf.init(ks, pass.toCharArray());
-        tmf.init(tks);
-        
-        ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
-        
-        serverSocket = (SSLServerSocket) ctx.getServerSocketFactory().createServerSocket(port);
-        serverSocket.setNeedClientAuth(true);//客户端认证
+	public void createVerifySSLServerSocket() throws Exception {
+		String type = "TLS";// 类型
+		String keyf = "sslserverkeys";// key文件路径
+		String trustf = "sslservertrust";// 信任证书库
+		String pass = "123456";// 密码
+		int port = 7070;// 端口
+		// 初始化上下文
+		SSLContext ctx = SSLContext.getInstance(type);
+
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+		TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+
+		KeyStore ks = KeyStore.getInstance("JKS");
+		KeyStore tks = KeyStore.getInstance("JKS");
+
+		// 载入keystore
+		InputStream keyfin = SSLServer.class.getClassLoader().getResourceAsStream(keyf);
+		InputStream trustfin = SSLServer.class.getClassLoader().getResourceAsStream(trustf);
+		ks.load(keyfin, pass.toCharArray());
+		tks.load(trustfin, pass.toCharArray());
+
+		kmf.init(ks, pass.toCharArray());
+		tmf.init(tks);
+
+		ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+
+		serverSocket = (SSLServerSocket) ctx.getServerSocketFactory().createServerSocket(port);
+		serverSocket.setNeedClientAuth(true);// 客户端认证
 	}
 
 	public SSLServer() throws Exception {
 		createVerifySSLServerSocket();
 	}
 
-	private void runServer() { 
-	 while (true) { 
-		 try { 
-			 System.out.println("Waiting for connection..."); 
-			 // 服务器端套接字进入阻塞状态，等待来自客户端的连接请求
-			 SSLSocket socket = (SSLSocket) serverSocket.accept(); 
-			
-			 // 获取服务器端套接字输入流
-			 BufferedReader input = new BufferedReader( 
-			        new InputStreamReader(socket.getInputStream())); 
-		 // 从输入流中读取客户端用户名和密码
-			 String userName = input.readLine(); 
-			 String password = input.readLine(); 
-			
-			 // 获取服务器端套接字输出流
-			 PrintWriter output = new PrintWriter( 
-			        new OutputStreamWriter(socket.getOutputStream())); 
+	private void runServer() {
+		while (true) {
+			try {
+				System.out.println("Waiting for connection...");
+				// 服务器端套接字进入阻塞状态，等待来自客户端的连接请求
+				SSLSocket socket = (SSLSocket) serverSocket.accept();
 
-		 // 对请求进行认证，如果通过则将保密内容发送给客户端
-			 if (userName.equals(USER_NAME) && password.equals(PASSWORD)) { 
-				 output.println("Welcome, " + userName); 
-				 output.println(SECRET_CONTENT); 
-			 } else { 
-				 output.println("Authentication failed, you have no access to server X..."); 
-			 } 
-		
-		 // 关闭流资源和套接字资源
-			 output.close(); 
-			 input.close(); 
-			 socket.close(); 
+				// 获取服务器端套接字输入流
+				BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				// 从输入流中读取客户端用户名和密码
+				String userName = input.readLine();
+				String password = input.readLine();
 
-		 } catch (IOException ioException) { 
-			 ioException.printStackTrace(); 
-		 } 
-	 } 
-}	public static void main(String args[]) throws Exception {
+				// 获取服务器端套接字输出流
+				PrintWriter output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+				// 对请求进行认证，如果通过则将保密内容发送给客户端
+				if (userName.equals(USER_NAME) && password.equals(PASSWORD)) {
+					output.println("Welcome, " + userName);
+					output.println(SECRET_CONTENT);
+				} else {
+					output.println("Authentication failed, you have no access to server X...");
+				}
+
+				// 关闭流资源和套接字资源
+				output.close();
+				input.close();
+				socket.close();
+
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String args[]) throws Exception {
 		SSLServer server = new SSLServer();
 		server.runServer();
 	}
